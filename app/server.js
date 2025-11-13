1326a9d52d3dd95084e96eb1ff4bce67376c0993
sdef2 <sdef2da1@gmail.com>
2025-11-13 17:21:04 +0900

    fix:Directory構造のリファクタリング

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const models = require("./models");
+const models = require("../models");
 const multer = require("multer");
 const upload = multer({
   storage: multer.diskStorage({
