8c89ee8f5cca90737d85fbbb9c21efab80ce6ce1
sdef2 <sdef2da1@gmail.com>
2025-11-14 10:51:25 +0900

    add,fix:img,uploadsの追加と修正

---
+++ features/image/routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const upload = require("./upload");
+
+const router = express.Router();
+
+router.post("/", upload.single("image"), (req, res) => {
+    if (!req.file) return res.status(400).send({ error: "ファイルがありません" });
+    res.send({ imageUrl: req.file.filename });
+});
+
+module.exports = router;
